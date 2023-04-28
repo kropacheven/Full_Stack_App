58a71066d8c57dd0d4408f6157f1ae2a25f17b49
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-28 10:22:36 +0500

    Add basic mark app to App.js (so far)

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,41 @@ function App() {
       })
   }, [ ]);
   return (
-    <div>
-
+    <div id="root">
+        <header>
+            <div className="wrap header--flex">
+                <h1 className="header--logo"><a href="index.html">Courses</a></h1>
+                <nav>
+                    <ul className="header--signedout">
+                        <li><a href="sign-up.html">Sign Up</a></li>
+                        <li><a href="sign-in.html">Sign In</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <main>
+            <div className="wrap main--grid">
+                <a className="course--module course--link" href="course-detail.html">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Build a Basic Bookcase</h3>
+                </a>
+                <a className="course--module course--link" href="course-detail.html">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Learn How to Program</h3>
+                </a>
+                <a className="course--module course--link" href="course-detail.html">
+                    <h2 className="course--label">Course</h2>
+                    <h3 className="course--title">Learn How to Test Programs</h3>
+                </a>
+                <a className="course--module course--add--module" href="create-course.html">
+                    <span className="course--add--title">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                        viewBox="0 0 13 13" className="add"><polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon></svg>
+                        New Course
+                    </span>
+                </a>
+            </div>
+        </main>
     </div>
   );
 }
