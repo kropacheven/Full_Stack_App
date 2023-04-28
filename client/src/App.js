a68fc51fa497a7babac02ab1a0418fe0c3ef45e9
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-04-28 10:02:39 +0500

    Set axios fetch request_require cors in api_make connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
-//import './App.css';
+import React, { useEffect, useState }  from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [courses, setCourses] = useState( [] );
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/courses')
+      .then(response => {
+        // handle success
+        console.log(response.data);
+      })
+      .catch( error => {
+        // handle error
+        console.log("Error fetching and parsing data", error);
+      })
+  }, [ ]);
   return (
     <div>
 
