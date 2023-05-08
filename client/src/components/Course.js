de268ad33ba49c9c35fbaf5c734b41e729241189
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-05-08 14:42:45 +0500

    Add Course component for one course only

---
+++ client/src/components/Course.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Course = () => {
+    return (
+        <a className="course--module course--link" href="course-detail.html">
+            <h2 className="course--label">Course</h2>
+            <h3 className="course--title">Build a Basic Bookcase</h3>
+        </a>
+    );
+}
+
+export default Course;
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,22 +1,12 @@
 import React from "react";
 
+import Course from "./Course";
 
 const Courses = () => {
     return (
         <main>
             <div className="wrap main--grid">
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Build a Basic Bookcase</h3>
-                </a>
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Learn How to Program</h3>
-                </a>
-                <a className="course--module course--link" href="course-detail.html">
-                    <h2 className="course--label">Course</h2>
-                    <h3 className="course--title">Learn How to Test Programs</h3>
-                </a>
+                <Course />
                 <a className="course--module course--add--module" href="create-course.html">
                     <span className="course--add--title">
                         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
