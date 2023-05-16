4743995fb59a2d38e62dfb85e6b8cbfa51100490
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-05-16 20:56:46 +0500

    Trying to make update text changable with useRef

--- client/src/components/UpdateCourseDesc.js
+++ client/src/components/UpdateCourseDesc.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 const UpdateCourseDesc = (props) => {
     let navigate = useNavigate();
 
+    const title = useRef();
+    const desc = useRef();
+    const time = useRef();
+    const material = useRef();
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        //let courseTitle = title.current.value;
+        //let courseDesc = desc.current.value;
+        //let courseTime = time.current.value;
+        //let courseMaterial = material.current.value;
         navigate(-1);
     };
 
@@ -17,23 +26,23 @@ const UpdateCourseDesc = (props) => {
     return (
         <div class="wrap">
             <h2>Update Course</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div class="main--flex">
                     <div>
                         <label for="courseTitle">Course Title</label>
-                        <input id="courseTitle" name="courseTitle" type="text" value={props.title}></input>
+                        <input id="courseTitle" name="courseTitle" type="text" value={props.title} ref={title}></input>
 
                         <p>By {props.firstName} {props.lastName}</p>
 
                         <label for="courseDescription">Course Description</label>
-                        <textarea id="courseDescription" name="courseDescription">{props.desc}</textarea>
+                        <textarea id="courseDescription" name="courseDescription" ref={desc}>{props.desc}</textarea>
                     </div>
                     <div>
                         <label for="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value={props.time}></input>
+                        <input id="estimatedTime" name="estimatedTime" type="text" value={props.time} ref={time}></input>
 
                         <label for="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded" name="materialsNeeded">{props.material}</textarea>
+                        <textarea id="materialsNeeded" name="materialsNeeded" ref={material}>{props.material}</textarea>
                     </div>
                 </div>
                 <button class="button" type="submit" onClick={handleSubmit}>Update Course</button><button class="button button-secondary" onClick={handleCancel}>Cancel</button>
