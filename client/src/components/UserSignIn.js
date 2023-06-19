30b6618b3c000154d89a614435497974d34bb710
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-19 15:34:46 +0500

    Activating cancel buttons on UserSignIn

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,7 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const UserSignIn = () => {
+    let navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const handleCancel = (e) => {
+        e.preventDefault();
+        navigate("/");
+    };
+
+
     return (
         <main>
         <div class="form--centered">
@@ -12,7 +25,7 @@ const UserSignIn = () => {
                 <input id="emailAddress" name="emailAddress" type="email" value=""></input>
                 <label for="password">Password</label>
                 <input id="password" name="password" type="password" value=""></input>
-                <button class="button" type="submit">Sign In</button><button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                <button class="button" type="submit" onClick={handleCancel}>Sign In</button><button class="button button-secondary" onClick={handleCancel}>Cancel</button>
             </form>
             <p>Don't have a user account? Click here to <NavLink to="/signup">sign up</NavLink>!</p>
             
