128376f1d7c050137d612a6da8a35a7678f5454a
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-23 00:00:37 +0500

    Make Header to react on signin

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,35 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+//Context
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
+
 const Header = () => {
+    const { authUser } = useContext(UserContext);
     return (
         <header>
             <div className="wrap header--flex">
                 <h1 className="header--logo"><NavLink to="/">Courses</NavLink></h1>
                 <nav>
-                    <ul className="header--signedout">
-                        <li><NavLink to="signup">Sign Up</NavLink></li>
-                        <li><NavLink to="signin">Sign In</NavLink></li>
-                    </ul>
+                    {
+                        authUser === null ?
+                            <>
+                                <ul className="header--signedout">
+                                    <li><NavLink to="signup">Sign Up</NavLink></li>
+                                    <li><NavLink to="signin">Sign In</NavLink></li>
+                                </ul>
+                            </>
+                            :
+                            <>
+                                <ul className="header--signedout">
+                                    <li><span>Welcome {authUser.firstName} {authUser.lastName}!</span></li>
+                                    <li><NavLink to="signout">Sign Out</NavLink></li>
+                                </ul>
+                            </>
+
+                    }
+
                 </nav>
             </div>
         </header>
