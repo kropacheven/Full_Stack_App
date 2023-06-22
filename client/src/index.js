2c55e516af99eb9f7e92e40ffc83fe2e01ef0a18
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-22 23:15:08 +0500

    add UserProvider in index.js as HOC

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,15 @@ import './index.css';
 import './styles/global.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
