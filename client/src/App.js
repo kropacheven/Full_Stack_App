796e2b1d132fa4d065046b3e6ec8f02e407e4347
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-23 23:24:38 +0500

    Add Private route to create and update components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // App tools:
-import React, { useEffect, useState }  from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import { Route, Routes } from 'react-router-dom';
@@ -15,10 +15,11 @@ import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 import UserSignOut from './components/UserSignOut';
 import NotFound from './components/NotFound';
+import PrivateRoute from './components/PrivateRoute';
 
 
 function App() {
-  const [courses, setCourses] = useState( [] );
+  const [courses, setCourses] = useState([]);
   //console.log(courses);
   useEffect(() => {
     axios.get('http://localhost:5000/api/courses')
@@ -26,29 +27,28 @@ function App() {
         // handle success
         setCourses(response.data);
       })
-      .catch( error => {
+      .catch(error => {
         // handle error
         console.log("Error fetching and parsing data", error);
       })
-  }, [ ]);
+  }, []);
 
   return (
     <div id="root">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Courses data={courses}/>} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Courses data={courses} />} />
+        <Route path="courses/:id" element={<CourseDetail data={courses} />} />
+        <Route path="signin" element={<UserSignIn />} />
+        <Route path="signup" element={<UserSignUp />} />
+        <Route path="signout" element={<UserSignOut />} />
+        <Route element={<PrivateRoute />}>
           <Route path="courses/create" element={<CreateCourse />} />
-          
-          <Route path="courses/:id" element={<CourseDetail data={courses}/>} /> 
-          <Route path="courses/:id/update" element={<UpdateCourse data={courses}/>} /> 
+          <Route path="courses/:id/update" element={<UpdateCourse data={courses} />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
 
-          <Route path="signin" element={<UserSignIn />} />
-          <Route path="signup" element={<UserSignUp />} />
-          <Route path="signout" element={<UserSignOut />} />
-
-          <Route path="*" element={<NotFound />}/>
-
-        </Routes>
+      </Routes>
     </div>
   );
 }
