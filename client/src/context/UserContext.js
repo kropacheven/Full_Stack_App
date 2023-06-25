508d1f4394b4a15617bb2d47599ff26e08d978bf
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-25 22:57:11 +0500

    make user persist on the page with cookie

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,12 +1,13 @@
 import { createContext, useState } from "react";
+import Cookies from "js-cookie"; // cookie
 
 //Creating new context
 const UserContext = createContext(null);
 
 //Creating context provider:
 export const UserProvider = (props) => {
-
-    const [authUser, setAuthUser] = useState(null);
+    const cookie = Cookies.get("authenticatedUser"); // cookie
+    const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null); // cookie
 
     // Sign-in function 
     const signIn = async (credentials) => {
@@ -26,6 +27,7 @@ export const UserProvider = (props) => {
             const user = await response.json();
             //console.log(`SUCCESS! ${user.firstName} is successfully signedin.`);
             setAuthUser(user);
+            Cookies.set("authenticatedUser", JSON.stringify(user), {expires: 1}); // cookie
             return user;
         } else if (response.status === 401) {
             return null;
@@ -37,6 +39,7 @@ export const UserProvider = (props) => {
     // Sign-out function:
     const signOut = () => {
         setAuthUser(null);
+        Cookies.remove("authenticatedUser"); //
     }
 
     // JSX of the component as Provider
