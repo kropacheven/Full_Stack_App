f8fabc274b5d9d7c878028589a23c62db7302c06
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-27 23:45:34 +0500

    Trying to make CourseDetail interact on auth user

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -7,15 +7,17 @@ const CourseDetail = (props) => {
     let { id } = useParams();
     let results = props.data;
     let courses = results.filter(course => course.id == id).map(course => <CourseDesc title={course.title} desc={course.description} time={course.estimatedTime} material={course.materialsNeeded}  firstName={course.owner.firstName} lastName={course.owner.lastName} key={course.id}/>);
-    
+    let theCourse = results.filter(course => course.id == id);
     return (
         <main>
             <div className="actions--bar">
+                {  console.log(theCourse) }
                 <div className="wrap">
                     <NavLink className="button" to="update">Update Course</NavLink>
                     <NavLink className="button" to="/">Delete Course</NavLink>
                     <NavLink className="button button-secondary" to="/">Return to List</NavLink>
                 </div>
+
             </div>
             {courses}
         </main>
