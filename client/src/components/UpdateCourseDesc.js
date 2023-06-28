9f45aa70198ccdfd9f5209e14a07db0b33c7cbda
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-06-28 18:25:56 +0500

    reformatting UpdateCourse from

--- client/src/components/UpdateCourseDesc.js
+++ client/src/components/UpdateCourseDesc.js
@@ -26,19 +26,40 @@ const UpdateCourseDesc = (props) => {
                 <div class="main--flex">
                     <div>
                         <label for="courseTitle">Course Title</label>
-                        <input id="courseTitle" name="courseTitle" type="text" value={props.title} ref={title}></input>
+                        <input
+                            id="courseTitle"
+                            name="courseTitle"
+                            type="text"
+                            value={props.title}
+                            ref={title} >
+                        </input>
 
                         <p>By {props.firstName} {props.lastName}</p>
 
                         <label for="courseDescription">Course Description</label>
-                        <textarea id="courseDescription" name="courseDescription" ref={desc}>{props.desc}</textarea>
+                        <textarea
+                            id="courseDescription"
+                            name="courseDescription"
+                            ref={desc}>
+                            {props.desc}
+                        </textarea>
                     </div>
                     <div>
                         <label for="estimatedTime">Estimated Time</label>
-                        <input id="estimatedTime" name="estimatedTime" type="text" value={props.time} ref={time}></input>
-
+                        <input
+                            id="estimatedTime"
+                            name="estimatedTime"
+                            type="text"
+                            value={props.time}
+                            ref={time}>
+                        </input>
                         <label for="materialsNeeded">Materials Needed</label>
-                        <textarea id="materialsNeeded" name="materialsNeeded" ref={material}>{props.material}</textarea>
+                        <textarea
+                            id="materialsNeeded"
+                            name="materialsNeeded"
+                            ref={material}>
+                            {props.material}
+                        </textarea>
                     </div>
                 </div>
                 <button class="button" type="submit" onClick={handleSubmit}>Update Course</button><button class="button button-secondary" onClick={handleCancel}>Cancel</button>
