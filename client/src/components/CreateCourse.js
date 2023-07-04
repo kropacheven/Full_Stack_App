180c6d12095b9f39ca7d99745c84a16114e7885e
kropacheven <81347649+kropacheven@users.noreply.github.com>
2023-07-04 14:21:37 +0500

    CreateCourse clean-up

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 //Context
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import UserContext from "../context/UserContext";
 
 
